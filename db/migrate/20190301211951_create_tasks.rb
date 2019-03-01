d06820aa23bd6a8678a3f3c95fa26a3c8937018e
Riley Pratt <rileyp33@gmail.com>
2019-03-01 17:55:25 -0500

    Initial DB setup and Foundation 6 install

---
+++ db/migrate/20190301211951_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.belongs_to :list, null: false
+      t.string :name, null: false
+      t.text :notes
+      t.timestamps
+    end
+  end
+end

aaefc8de678d709d1368970cf434b77baa77c04e
Riley Pratt <rileyp33@gmail.com>
2019-03-05 15:32:46 -0500

    Delete lists from list manager

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,8 +10,25 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    target_id = destroy_list_params["id"]
+
+    if !current_user.nil? && current_user == List.find(target_id).user
+      List.find(target_id).delete
+      render json: List.where(user_id: current_user.id), each_serializer: ListIndexSerializer
+    else
+      render json: { error: @list.errors.full_messages }, status: :unprocessable_entity
+    end
+
+  end
+
   private
   def list_params
     params.require(:list).permit(:name)
   end
+
+  def destroy_list_params
+    params.require(:list).permit(:id)
+  end
+
 end
